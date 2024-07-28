92149307a097a9f75f39816bfc13e71e95f4cd0f
hc precog <chao921125@sina.com>
2024-07-28 13:02:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
     // zyplayer
     // https://cdn.jsdelivr.net/gh/chao921125/source@2.0.0/zyplayer/index.json
     // 洛雪音乐助手
-    // https://cdn.jsdelivr.net/gh/chao921125/source@2.0.0/music/****.js
+    // https://cdn.jsdelivr.net/gh/chao921125/source@2.0.0/lx_music/****.js
 })(window);
