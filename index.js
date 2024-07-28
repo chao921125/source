571c938d7b2ce8bbc80c95a8449b08874c50c39e
hc precog <chao921125@sina.com>
2024-07-28 13:02:12 +0800

    更改名称

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 ((dom) => {
     // zyplayer
-    // https://cdn.jsdelivr.net/gh/chao921125/source@1.0.0/zyplayer/index.json
+    // https://cdn.jsdelivr.net/gh/chao921125/source@2.0.0/zyplayer/index.json
+    // 洛雪音乐助手
+    // https://cdn.jsdelivr.net/gh/chao921125/source@2.0.0/music/****.js
 })(window);
