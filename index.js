30c0fdc44eb14f0a83fdf3dd0f105b14edc36ea4
hc precog <chao921125@sina.com>
2024-08-12 23:12:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 ((dom) => {
     // zyplayer
-    // https://cdn.jsdelivr.net/gh/chao921125/source@2.0.0/zyplayer/index.json
+    // https://cdn.jsdelivr.net/gh/chao921125/source@3.0.0/zyplayer/index.json
     // 洛雪音乐助手
-    // https://cdn.jsdelivr.net/gh/chao921125/source@2.0.0/lxmusic/****.js
+    // https://cdn.jsdelivr.net/gh/chao921125/source@3.0.0/lxmusic/****.js
 })(window);
