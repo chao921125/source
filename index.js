32e544f5de2b178d868e1e543157a683999608cc
hc precog <chao921125@sina.com>
2024-08-13 21:25:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,6 @@
     // https://cdn.jsdelivr.net/gh/chao921125/source@3.0.0/zyplayer/index.json
     // 洛雪音乐助手
     // https://cdn.jsdelivr.net/gh/chao921125/source@3.0.0/lxmusic/****.js
+    // cimoc
+    // https://cdn.jsdelivr.net/gh/chao921125/source@3.0.0/cimoc/****.json
 })(window);
