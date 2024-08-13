6c7f406ad7a7283ae9e1b50c0828f53354f977ae
hc precog <chao921125@sina.com>
2024-08-13 21:01:58 +0800

    Update index.js

--- legado/index.js
+++ legado/index.js
@@ -3,6 +3,7 @@
 //     https://www.e-yck.top/yuedu/index/index.html
 //     https://gedoor.github.io/
 //     https://www.gitlink.org.cn/yi-c/yd
+//     https://legado.aoaostar.com/
 //     https://mirror.ghproxy.com/https://raw.githubusercontent.com/shidahuilang/shuyuan/shuyuan/good.json
 //     http://sy.legado1.top/sy.php/a70dce0cc0b32cd5120e568603b4888d.json
 //     http://sy.legado1.top/sy.php/a921c98826526e13a8be13f4cea49d93.json&#8203;
